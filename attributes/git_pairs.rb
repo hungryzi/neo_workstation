d8abf04e30828054afe03abf62e6fa2861f18cd2
Winston <winston.yongwei@gmail.com>
2012-12-20 11:42:32 +0800

    Overwrite git_pair authors in pivotal_workstation.

---
+++ attributes/git_pairs.rb
@@ -0,0 +1,13 @@
+node.default["git_pairs_domain"]  = "neo.com"
+
+node.default["git_pairs_authors"] = [
+  {"initials" => "as" , "name" => "Abhaya Shenoy"},
+  {"initials" => "dg" , "name" => "Diego Garcia"},
+  {"initials" => "mk" , "name" => "Mohan Krishnan"},
+  {"initials" => "mm" , "name" => "Mike Mazur"},
+  {"initials" => "tm" , "name" => "Tomek Maskowski"},
+  {"initials" => "ts" , "name" => "Tommy Sullivan"},
+  {"initials" => "wei", "name" => "Wei Lu"},
+  {"initials" => "wt" , "name" => "Winston Teo"},
+  {"initials" => "zi" , "name" => "Huong Vu (Zi)"}
+]
