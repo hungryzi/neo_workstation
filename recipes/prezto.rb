1294fa01cfc4eb73c303a4871d5fd6868a63ca0d
Winston <winston.yongwei@gmail.com>
2012-12-20 15:21:45 +0800

    Update prezto recipe.

--- recipes/prezto.rb
+++ recipes/prezto.rb
@@ -1,25 +1,23 @@
 include_recipe "pivotal_workstation::git"
 include_recipe "pivotal_workstation::zsh"
 
-
 ::PREZTO_DIR = ::File.expand_path(".zprezto", WS_HOME)
 
-g = git "#{Chef::Config[:file_cache_path]}/prezto" do
+git_clone   = git "#{Chef::Config[:file_cache_path]}/prezto" do
   repository 'https://github.com/sorin-ionescu/prezto.git'
-  #revision bash_it_version
   destination "#{Chef::Config[:file_cache_path]}/prezto"
   action :nothing
 end
 
-g.run_action(:sync)
+git_clone.run_action(:sync)
 
-e = execute "Copying prezto's .git to #{PREZTO_DIR}" do
+dotgit_copy = execute "Copying prezto's .git to #{PREZTO_DIR}" do
   command "rsync -axSH #{Chef::Config[:file_cache_path]}/prezto/ #{PREZTO_DIR}"
   user WS_USER
   action :nothing
 end
 
-e.run_action(:run)
+dotgit_copy.run_action(:run)
 
 ['zlogin', 'zlogout', 'zpreztorc', 'zprofile', 'zshenv', 'zshrc'].each do |zfile|
   file "#{WS_HOME}/.#{zfile}" do
@@ -29,9 +27,9 @@ e.run_action(:run)
   end
 end
 
-e = execute "Change the default shell to zsh" do
+change_shell = execute "Change the default shell to zsh" do
   command "chsh -s /bin/zsh #{WS_USER}"
   action :nothing
 end
 
-e.run_action(:run)
+change_shell.run_action(:run)
