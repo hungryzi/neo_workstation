c33855eefab902535249dccd7503c68c379cd604
Winston <winston.yongwei@gmail.com>
2012-12-20 12:23:16 +0800

    Add metadata.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Neo"
+maintainer_email  "accounts@neo.com"
+license           "MIT"
+description       "Configure OSX workstation"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+supports          "mac_os_x"
+depends           "dmg"
