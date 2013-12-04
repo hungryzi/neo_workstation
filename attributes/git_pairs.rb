01ecde078c9bbc2991f4397fa6c53db0ad6e8077
Zi <hungryzi@gmail.com>
2013-12-04 15:35:01 +0800

    Update git_pairs

--- attributes/git_pairs.rb
+++ attributes/git_pairs.rb
@@ -2,12 +2,12 @@ node.set["git_pairs_domain"]  = "neo.com"
 
 node.set["git_pairs_authors"] = [
   {"initials" => "as" , "name" => "Abhaya Shenoy"},
+  {"initials" => "ay" , "name" => "Alan Yeo"},
   {"initials" => "dg" , "name" => "Diego Garcia"},
+  {"initials" => "gh" , "name" => "Gabe Hollombe"},
   {"initials" => "mk" , "name" => "Mohan Krishnan"},
-  {"initials" => "mm" , "name" => "Mike Mazur"},
   {"initials" => "tm" , "name" => "Tomek Maszkowski"},
   {"initials" => "ts" , "name" => "Tommy Sullivan"},
   {"initials" => "wei", "name" => "Wei Lu"},
-  {"initials" => "wt" , "name" => "Winston Teo"},
   {"initials" => "zi" , "name" => "Zi"}
 ]
