2afc69be23453b6fa63b9ca3e6f09459c4e473a8
Zi <hungryzi@gmail.com>
2013-12-05 16:27:54 +0800

    Delete Spectacle and Skitch recipes as we moved to homebrew_casks

--- attributes/skitch.rb
+++
@@ -1,2 +0,0 @@
-node.default["skitch_download_uri"]="http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.1.zip"
-
--- attributes/spectacle.rb
+++
@@ -1,1 +0,0 @@
-node.default["spectacle_download_uri"]="https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.4.zip"
